f30d77ea86d76e0045e6b18387382d13ca42479f
Kamo3131 <kacpermar2004@gmail.com>
2025-06-17 19:53:38 +0200

    Everything is working, yuppie!

--- src/main/java/common/messages/FileUpdateMessage.java
+++ src/main/java/common/messages/FileUpdateMessage.java
@@ -2,4 +2,4 @@ package common.messages;
 
 import java.io.Serializable;
 
-public record FileUpdateMessage(String userID, String username, String filename, long date, double size) implements Serializable {}
+public record FileUpdateMessage(String userID, String username, String filename, long date, long size) implements Serializable {}
